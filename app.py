75a02f9c0a99a5862dcd528e573edfd57093fde7
Parker Clark <tundravol2@gmail.com>
2025-01-26 22:58:47 -0600

    Added folder structure, dependencies, and some other stuff.

---
+++ app.py
@@ -0,0 +1,15 @@
+'''----------------- 
+# Title: app.py
+# Author: Parker Clark
+# Date: 1/26/2025
+# Description: Main API endpoint for the application.
+-----------------'''
+
+#---Imports---#
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    pass
