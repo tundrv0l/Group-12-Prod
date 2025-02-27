36ff28b8144597f080ca49309bb556e3f10bba1b
tony-mathias-buchanan <tmb0033@uah.edu>
2025-02-26 18:31:32 -0600

    create Warshall_solver.py
    given a 2D array, outputs a series of 2D arrays, performs the Warshall solver that was taught in the book, so it doesn't actually show how far apart they are.

---
+++ backend/solvers/Warshall_solver.py
@@ -0,0 +1,52 @@
+import math
+import json
+
+def solve(matrix):
+    V = len(matrix)
+    print("beginning graph:")
+    print(matrix)
+    print("\n")
+    print("intermediate graphs")
+    matriix = []
+    
+    for k in range(V):
+        for i in range (V):
+            for j in range(V):
+                if matrix[i][j] == "0" and matrix[i][k] == "1" and matrix[k][j] == "1":
+                    matrix[i][j] = "1"
+        newMatr = []
+        for i in range(V):
+            line = []
+            for j in range(V):
+                line.append(matrix[i][j])
+            newMatr.append(line)
+        matriix.append(newMatr)
+
+    # Prepare the truth table as a JSON object
+    truth_table = {
+        "matricies": []
+    }
+
+    # Add the rows to the truth table
+    for row in matriix:
+        truth_table["matricies"].append(row)
+
+    return json.dumps(truth_table)
+
+m = input("you know the deal")
+ms = m.replace(" ","").split(",")
+matr = []
+tempList = []
+
+x=0
+y=0
+for n in ms:
+    tempList.append(n)
+    y += 1
+    if y == math.sqrt(ms.__len__()):
+        y = 0
+        x += 1
+        matr.append(tempList)
+        tempList = []
+
+print(solve(matr))
