365fb3ea7237a6d233f4973b84888d5822292020
tony-mathias-buchanan <tmb0033@uah.edu>
2025-04-07 15:13:51 -0500

    fixes 4.4 to be more to his liking

--- backend/solvers/compisitions_solver.py
+++ backend/solvers/compisitions_solver.py
@@ -1,30 +1,58 @@
-#File: compisitions_solver.py
-#Author: Mathias Buchanan
-#Solves: 5.4, no problems associated
-
 import json
+
+def strike(text):
+    return ''.join(c + '\u0336' for c in str(text))
 
 def solve(setOneSize, setTwoSize):
     if isinstance(setOneSize, str):
         setOneSize = int(setOneSize)
     if isinstance(setTwoSize, str):
         setTwoSize = int(setTwoSize)
-    permutations = 1
-    compisitions = 1
 
-    i = 1 + setOneSize - setTwoSize
-    while i <= setOneSize:
-        permutations = permutations * i
-        i += 1
-    
-    compisitions = permutations
+    i = setOneSize
+    j = setOneSize - setTwoSize
 
-    for i in range(setTwoSize):
-        compisitions /= (i + 1)
+    # Permutations string
+    k = i - 1
+    permutations = str(i)
+    while k > j:
+        permutations += " * " + str(k)
+        k -= 1
+
+    # Composition logic
+    l = list(range(2, setTwoSize + 1)) if j > setTwoSize else list(range(2, j + 1))
+    if j <= setTwoSize:
+        j = setTwoSize
+
+    k = i
+    composition_parts = []
+
+    while k > j:
+        printed_num = k
+        part = []
+
+        for idx in reversed(range(len(l))):
+            divisor = l[idx]
+            if isinstance(divisor, int) and printed_num % divisor == 0:
+                part.append(strike(str(printed_num)))
+                printed_num //= divisor
+                l[idx] = strike(str(divisor))  # cross it out
+
+        part.append(str(printed_num))
+        composition_parts.append(" ".join(part))
+        k -= 1
+
+    compositions = " * ".join(composition_parts)
+
+    compositions += " / (" + " * ".join(str(x) for x in l) + ")"
 
     result = {
         "perm": permutations,
-        "comp": compisitions
+        "comp": compositions
     }
 
     return json.dumps(result)
+
+# Example Usage
+output = solve(55, 50)
+print(output)
