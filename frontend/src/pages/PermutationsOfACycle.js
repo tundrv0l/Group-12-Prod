9bb90087a92cb4feb60a83a5c699951fa399094e
jdww5567 <112447476+jdww5567@users.noreply.github.com>
2025-04-16 03:05:18 -0500

    disjoint cycles page: rewrite description

--- frontend/src/pages/PermutationsOfACycle.js
+++ frontend/src/pages/PermutationsOfACycle.js
@@ -211,10 +211,10 @@ const Description = () => {
         />
         <Text weight="bold" margin={{"bottom": "small"}}>Cycle Form</Text>
         <LatexLine
-          string="The cycle form of a permutation expresses the permutation as a composition of disjoint cycles, where each element maps to the element to its right (wrapping at the end). Every permutation on finite sets, besides the identity permutation can be represented this way (with a single element cycle (a) representing the identity, for output purposes). Example: $$ (1 2 3)\circ (4 5)$$."
+          string="The cycle form of a permutation expresses the permutation as a composition of disjoint cycles, where each element maps to the element to its right (wrapping at the end). Every permutation on finite sets, besides the identity permutation, can be represented this way (with a single element cycle (a) representing the identity, for output purposes). Example: $$ (1\ 2\ 3)\circ (4\ 5)$$."
         />
         <LatexLine
-          string="Enter your perumtation of $S$ in array form below."
+          string="Enter your permutation of $S$ in array form below."
         />
       </div>
     );
